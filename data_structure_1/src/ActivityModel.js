af747f178865fcc1283167c7a59fca1be7401f32
kangxiaoya <1257333020kxy@gmail.com>
2014-05-09 16:17:27 +0800

    1 should first activity was created on creating

---
+++ data_structure_1/src/ActivityModel.js
@@ -0,0 +1,16 @@
+function Activity(activity_name){
+    this.name=activity_name;
+    this.sign_ups=[];
+    this.bids=[];
+}
+
+Activity.prototype.create=function(){
+  var activities=JSON.parse(localStorage.getItem('activities'))
+    activities.unshift(this)
+    localStorage.setItem("activities",JSON.stringify(activities))
+}
+
+Activity.prototype.active=function(){
+    localStorage.current_activity=this.name
+
+}
---
+++ data_structure_1/src/BidModel.js
@@ -0,0 +1,3 @@
+/**
+ * Created by yaya on 14-5-9.
+ */
