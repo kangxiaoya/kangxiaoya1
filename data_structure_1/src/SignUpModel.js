34089d67f55f48dc27a831b31a0ab488cd2134c7
kangxiaoya <1257333020kxy@gmail.com>
2014-05-11 13:26:41 +0800

    should one sms with signing up content sign up failed when user has signed

--- data_structure_1/src/SignUpModel.js
+++ data_structure_1/src/SignUpModel.js
@@ -5,14 +5,19 @@ SignUp.activity_sign_up_sms = function (sms_json) {
     if (localStorage.is_signing_up == "true") {
         var activities = JSON.parse(localStorage.activities);
         var current_activity = localStorage.current_activity;
+        var sign_ups = [];
         var sign_up = {};
         sign_up['name'] = sms_json.messages[0].message.substr(2).replace(/^\s+/g, '');
         sign_up['phone'] = sms_json.messages[0].phone;
+        if (_.find(sign_ups, function (the_sign_up) {
+            return the_sign_up['phone'] == sign_up['phone']
+        }) == undefined) {
+            sign_ups.push(sign_up);
+        }
         activities = _.map(activities, function (activity) {
-            activity['name'] = current_activity ? activity['sign_ups'].push(sign_up) : '';
+            activity['name'] = current_activity ? activity['sign_ups'] = sign_ups : '';
             return activity;
         });
         localStorage.setItem('activities', JSON.stringify(activities))
     }
-}
-
+};
